4b559322875dbfa1b33860a1a7fcb5e720b6b294
robert.cioltea <robertcioltea@gmail.com>
2023-05-12 21:00:49 +0300

    Maui project base

---
+++ maui/InterestPointClient/InterestPointClient/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace InterestPointClient;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ maui/InterestPointClient/InterestPointClient/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace InterestPointClient;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
