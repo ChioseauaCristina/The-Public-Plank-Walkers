fbe1b63be6fda93bde7048d09072d72b12181e34
Dobre Andrei <andrei.dobr3@gmail.com>
2023-05-13 00:26:23 +0300

    Created Login Page

---
+++ maui/InterestPointClient/InterestPointClient/Routes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestPointClient
+{
+    internal class Routes
+    {
+        public static string Main { get; } = "///: /main";
+
+        public static string Login { get; } = "///: /login";
+
+    }
+}
