f088a447874d6ed19e71f7bff61a34c38960f873
noemi.csillag <noemi269@yahoo.com>
2023-05-13 23:15:09 +0300

    added QueController and ObjectDetectionController

---
+++ server/PointsOfInterestAPI/PointsOfInterestAPI/Controllers/DetectionObjectController.cs
@@ -0,0 +1,62 @@
+﻿using Backend.Data.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace PointsOfInterestAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class DetectionObjectController : ControllerBase
+    {
+        private readonly PoIDbContext dbContext;
+        public DetectionObjectController(PoIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        [EnableCors("AllowOrigin")]
+        public async Task<IActionResult> GetDetectionObjects()
+        {
+            return Ok(await dbContext.DetectionObjects.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDETECTIONOBJECT(DetectionObject detectionObject)
+        {
+            Random rnd = new Random();
+            detectionObject.Id = rnd.Next();
+            await dbContext.DetectionObjects.AddAsync(detectionObject);
+            await dbContext.SaveChangesAsync();
+            return Ok(detectionObject);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateDETECTIONOBJECT(int id, DetectionObject detectionObject)
+        {
+            var _detectionObject = await dbContext.DetectionObjects.FindAsync(id);
+            if (_detectionObject != null)
+            {
+                _detectionObject.ImagePath = detectionObject.ImagePath;
+                //MapEverything else
+                await dbContext.SaveChangesAsync();
+                return Ok(_detectionObject);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteContact(int id)
+        {
+            var _detectionObject = await dbContext.DetectionObjects.FindAsync(id);
+            if (_detectionObject != null)
+            {
+                dbContext.Remove(_detectionObject);
+                dbContext.SaveChanges();
+                return Ok(_detectionObject);
+            }
+            return NotFound();
+        }
+    }
+}
+}
---
+++ server/PointsOfInterestAPI/PointsOfInterestAPI/Controllers/QueController.cs
@@ -0,0 +1,62 @@
+﻿using Backend.Data.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PointsOfInterestAPI.Entities;
+
+namespace PointsOfInterestAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class QueController : ControllerBase
+    {
+        private readonly PoIDbContext dbContext;
+        public QueController(PoIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        [EnableCors("AllowOrigin")]
+        public async Task<IActionResult> GetQues()
+        {
+            return Ok(await dbContext.Ques.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddQUE(Que que)
+        {
+            Random rnd = new Random();
+            que.Id = rnd.Next();
+            await dbContext.Ques.AddAsync(que);
+            await dbContext.SaveChangesAsync();
+            return Ok(que);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateQUE(int id, Que que)
+        {
+            var _que = await dbContext.Ques.FindAsync(id);
+            if (_que != null)
+            {
+                _que.DetectionObject = que.DetectionObject;
+                //MapEverything else
+                await dbContext.SaveChangesAsync();
+                return Ok(_que);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteContact(int id)
+        {
+            var _que = await dbContext.Ques.FindAsync(id);
+            if (_que != null)
+            {
+                dbContext.Remove(_que);
+                dbContext.SaveChanges();
+                return Ok(_que);
+            }
+            return NotFound();
+        }
+    }
+}
