35bb37267b17b4c9624b6750f998ce6441ce2f0e
robert.cioltea <robertcioltea@gmail.com>
2023-05-13 18:27:04 +0300

    login page added

--- maui/InterestPointClient/InterestPointClient/MainPage.xaml.cs
+++ maui/InterestPointClient/InterestPointClient/MainPage.xaml.cs
@@ -2,20 +2,15 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
+
+
+
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-	}
 
-    protected override void OnNavigatedTo(NavigatedToEventArgs args)
-    {
-        base.OnNavigatedTo(args);
-    }
 }
 
