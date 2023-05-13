a94b34aa60f4b5ddf146924b663728e4abbe3b54
Andrei Dobre <andrei.dobr3@gmail.com>
2023-05-14 01:49:04 +0300

    Added Brand New UsersController

--- server/PointsOfInterestAPI/PointsOfInterestAPI/Auth/UsersController.cs
+++ server/PointsOfInterestAPI/PointsOfInterestAPI/Auth/UsersController.cs
@@ -1,6 +1,8 @@
 
 
+using Backend.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace PointsOfInterestAPI.Auth
 {
@@ -14,7 +16,80 @@ namespace PointsOfInterestAPI.Auth
         {
             _dbContext = dbContext;
         }
+
+        [HttpPost]
+        [Route("/login")]
+        public async Task<ActionResult<User>> Login(LoginRequest request)
+        {
+            var user = await _dbContext.Users.FindAsync(request.Username);
+            if (user is null)
+            {
+                return Unauthorized("Username not found");
+            }
+
+            if (!user.Password.Equals(request.Password))
+            {
+                return Unauthorized("Incorrect password");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Register(RegisterRequest request)
+        {
+            var fuser = await _dbContext.Users.FindAsync(request.Username);
+            if (fuser is not null)
+            {
+                return BadRequest("Username already in use");
+            }
+
+            Random rand = new Random();
+            var user = new User
+            {
+                Id = rand.Next(),
+                Username = request.Username,
+                Password = request.Password,
+                Role = request.Role
+            };
+
+            var result = await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return Ok(await _dbContext.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get([FromRoute] string id)
+        {
+            var user = await _dbContext.Users.FindAsync(id);
+            if (user is null)
+            {
+                return NotFound("User not found");
+            }
+
+            return Ok(user);
+        }
         
-        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete([FromRoute] string id)
+        {
+            var user = await _dbContext.Users.FindAsync(id);
+            if (user is null)
+            {
+                return NotFound("User not found");
+            }
+
+            var result = _dbContext.Users.Remove(user);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(user);
+        } 
     }
 }
