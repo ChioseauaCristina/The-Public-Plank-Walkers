f7458f908bf550d2c0945acd5f5966a240dc0cbd
Andrei Dobre <andrei.dobr3@gmail.com>
2023-05-14 01:24:26 +0300

    Resolved conflicts

--- server/PointsOfInterestAPI/PointsOfInterestAPI/Auth/UsersController.cs
+++ server/PointsOfInterestAPI/PointsOfInterestAPI/Auth/UsersController.cs
@@ -1,11 +1,6 @@
-using Microsoft.AspNetCore.Identity;
+
+
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace PointsOfInterestAPI.Auth
 {
@@ -13,56 +8,13 @@ namespace PointsOfInterestAPI.Auth
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        private readonly UserManager<User> userManager;
-        private readonly IConfiguration _configuration;
+        private readonly PoIDbContext _dbContext;
 
-        public UsersController(UserManager<User> userManager, IConfiguration configuration)
+        public UsersController(PoIDbContext dbContext)
         {
-            this.userManager = userManager;
-            _configuration = configuration;
+            _dbContext = dbContext;
         }
-
-        [HttpPost]
-        public async Task<ActionResult<LoginResponse>> Login(LoginRequest request)
-        {
-            var user = await userManager.FindByNameAsync(request.Username);
-            if (user == null)
-            {
-                return Unauthorized("Username not found");
-            }
-
-            var wrongPass = !await userManager.CheckPasswordAsync(user, request.Password);
-            if (wrongPass)
-            {
-                return Unauthorized("Incorrect Password");
-            }
-
-            var userRoles = await userManager.GetRolesAsync(user);
-
-            if (userRoles == null)
-            {
-                await userManager.AddToRoleAsync(user, Utils.Roles.Client);
-            }
-
-            var authClaims = new List<Claim>
-            {
-                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
-                new("id", user.Id)
-            };
-
-            authClaims.AddRange(userRoles.Select(role => new Claim("role", role)));
-
-            var authSigninKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-
-            var token = new JwtSecurityToken(
-                expires: DateTime.Now.AddDays(1),
-                claims: authClaims,
-                signingCredentials: new SigningCredentials(authSigninKey,SecurityAlgorithms.HmacSha256)
-            );
-
-            return Ok(new LoginResponse { 
-                
-            });
+        
+        
     }
 }
